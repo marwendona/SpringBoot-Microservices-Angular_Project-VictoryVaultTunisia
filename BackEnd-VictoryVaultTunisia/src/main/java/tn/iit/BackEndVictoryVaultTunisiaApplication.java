18a6dbfabf2105b5831b9d4800d60d050b052e84
Marwen Kammoun <marwenkammoun3@gmail.com>
2023-11-15 17:48:33 +0100

    first commit

---
+++ BackEnd-VictoryVaultTunisia/src/main/java/tn/iit/BackEndVictoryVaultTunisiaApplication.java
@@ -0,0 +1,13 @@
+package tn.iit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndVictoryVaultTunisiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndVictoryVaultTunisiaApplication.class, args);
+	}
+
+}
