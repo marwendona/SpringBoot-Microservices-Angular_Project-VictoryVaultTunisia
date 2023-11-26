1cffbce126481b91cbd1eab7397f9686f9aaee80
Marwen Kammoun <marwenkammoun3@gmail.com>
2023-11-26 15:01:53 +0100

    create Season Service & Matches Service

---
+++ BackEnd-MatchesService/src/main/java/tn/iit/BackEndMatchesServiceApplication.java
@@ -0,0 +1,13 @@
+package tn.iit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndMatchesServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackEndMatchesServiceApplication.class, args);
+    }
+
+}
