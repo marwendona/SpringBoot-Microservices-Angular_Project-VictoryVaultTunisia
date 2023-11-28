fb5a4d21e1007e938c174a3b229958b5f814406f
mohamed youssef ben chehida <53410930+youssefbenchehida@users.noreply.github.com>
2023-11-28 14:03:22 +0100

    season service created entites

--- BackEnd-SeasonService/src/main/java/tn/iit/entity/Round.java
+++ BackEnd-SeasonService/src/main/java/tn/iit/entity/Round.java
@@ -4,8 +4,13 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.extern.jackson.Jacksonized;
 
+import java.math.BigInteger;
+
 @Data
 @Builder
 @Jacksonized
 public class Round {
+    private String Name;
+    private BigInteger roundNumber;
+
 }
--- BackEnd-SeasonService/src/main/java/tn/iit/entity/Standing.java
+++ BackEnd-SeasonService/src/main/java/tn/iit/entity/Standing.java
@@ -4,8 +4,13 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.extern.jackson.Jacksonized;
 
+import java.math.BigInteger;
+import java.util.List;
+
 @Data
 @Builder
 @Jacksonized
 public class Standing {
+    private BigInteger rank;
+    private BigInteger score;
 }
