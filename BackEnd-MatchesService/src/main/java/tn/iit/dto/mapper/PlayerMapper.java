12ae80eec1a21618e51d44c9146de35035b80b35
mohamed youssef ben chehida <53410930+youssefbenchehida@users.noreply.github.com>
2023-12-20 20:34:31 +0100

    added player mapper

---
+++ BackEnd-MatchesService/src/main/java/tn/iit/dto/mapper/PlayerMapper.java
@@ -0,0 +1,23 @@
+package tn.iit.dto.mapper;
+
+import tn.iit.dto.PlayerDto;
+import tn.iit.entity.Player;
+
+public class PlayerMapper {
+    public static Player toPlayer(PlayerDto dto) {
+        Player player = new Player();
+        player.setId(dto.getId());
+        player.setFirstName(dto.getFirstName());
+        player.setLastName(dto.getLastName());
+        player.setNationality(dto.getNationality());
+        return player;
+    }
+    public static PlayerDto toPlayerDto(Player player) {
+        return PlayerDto.builder().
+                id(player.getId()).
+                firstName(player.getFirstName()).
+                lastName(player.getLastName()).
+                nationality(player.getNationality()).
+                build();
+    }
+}
