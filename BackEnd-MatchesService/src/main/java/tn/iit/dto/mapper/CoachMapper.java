b7aae3e220ccda508bc55d7331814fc6cec1ecae
mohamed youssef ben chehida <53410930+youssefbenchehida@users.noreply.github.com>
2023-12-20 17:58:52 +0100

    created CoachMapper wip

---
+++ BackEnd-MatchesService/src/main/java/tn/iit/dto/mapper/CoachMapper.java
@@ -0,0 +1,23 @@
+package tn.iit.dto.mapper;
+
+import tn.iit.dto.CoachDto;
+import tn.iit.entity.Coach;
+
+public class CoachMapper {
+    public static Coach toCoach(CoachDto coachDto) {
+        Coach coach = new Coach();
+        coach.setFirstName(coachDto.getFirstName());
+        coach.setLastName(coachDto.getLastName());
+        coach.setNationality(coachDto.getNationality());
+        return coach;
+    }
+
+    public static CoachDto toCoachDto(Coach coach) {
+        return CoachDto.builder().
+                firstName(coach.getFirstName()).
+                lastName(coach.getLastName()).
+                nationality(coach.getNationality()).
+                build();
+
+    }
+}
