be53222c36db3b516ee744b3d7314ad512e41446
mohamed youssef ben chehida <53410930+youssefbenchehida@users.noreply.github.com>
2023-12-20 18:00:50 +0100

    implemented Coach mapper in CoachController

--- BackEnd-MatchesService/src/main/java/tn/iit/controller/CoachController.java
+++ BackEnd-MatchesService/src/main/java/tn/iit/controller/CoachController.java
@@ -1,10 +1,10 @@
 package tn.iit.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import tn.iit.dto.CoachDto;
+import tn.iit.dto.mapper.CoachMapper;
 import tn.iit.entity.Coach;
 import tn.iit.service.CoachService;
 
@@ -22,7 +22,7 @@ public class CoachController {
     public ResponseEntity<CoachDto> getCoachById(@PathVariable Long id) {
         Coach coach = coachService.getCoachById(id);
         if (coach != null) {
-            CoachDto coachDto = convertToDto(coach);
+            CoachDto coachDto = CoachMapper.toCoachDto(coach);
             return ResponseEntity.ok(coachDto);
         } else {
             return ResponseEntity.notFound().build();
@@ -31,9 +31,9 @@ public class CoachController {
 
     @PostMapping
     public ResponseEntity<CoachDto> createCoach(@RequestBody CoachDto coachDto) {
-        Coach coach = convertToEntity(coachDto);
+        Coach coach = CoachMapper.toCoach(coachDto);
         Coach createdCoach = coachService.createCoach(coach);
-        CoachDto createdCoachDto = convertToDto(createdCoach);
+        CoachDto createdCoachDto = CoachMapper.toCoachDto(createdCoach);
         return ResponseEntity.status(HttpStatus.CREATED).body(createdCoachDto);
     }
 
@@ -41,10 +41,10 @@ public class CoachController {
     public ResponseEntity<CoachDto> updateCoach(@PathVariable Long id, @RequestBody CoachDto coachDto) {
         Coach existingCoach = coachService.getCoachById(id);
         if (existingCoach != null) {
-            Coach coach = convertToEntity(coachDto);
+            Coach coach = CoachMapper.toCoach(coachDto);
             coach.setId(id);
             Coach updatedCoach = coachService.updateCoach(coach);
-            CoachDto updatedCoachDto = convertToDto(updatedCoach);
+            CoachDto updatedCoachDto = CoachMapper.toCoachDto(updatedCoach);
             return ResponseEntity.ok(updatedCoachDto);
         } else {
             return ResponseEntity.notFound().build();
