0a3643fc1586e14c4a7f3a9c35bcdf0c81523df9
mohamed youssef ben chehida <53410930+youssefbenchehida@users.noreply.github.com>
2023-12-21 17:28:10 +0100

    added Team Mapper

--- BackEnd-MatchesService/src/main/java/tn/iit/dto/mapper/TeamMapper.java
+++ BackEnd-MatchesService/src/main/java/tn/iit/dto/mapper/TeamMapper.java
@@ -1,16 +1,24 @@
 package tn.iit.dto.mapper;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import tn.iit.dto.TeamDto;
 import tn.iit.entity.Team;
+import tn.iit.service.CoachService;
 
 public class TeamMapper {
+
+    private static CoachService coachService;
+
+    public TeamMapper(CoachService coachService) {
+        TeamMapper.coachService = coachService;
+    }
 
     public static TeamDto toTeamDto(Team team) {
         return TeamDto.builder().
                 id(team.getId()).
                 name(team.getName()).
                 players(team.getPlayers().stream().map(PlayerMapper::toPlayerDto).toList()).
-                coach(CoachMapper.toCoachDto(team.getCoach())).
+                coachId(team.getCoach().getId()).
                 build();
     }
     public static Team toTeam(TeamDto dto) {
@@ -18,7 +26,7 @@ public class TeamMapper {
         team.setId(dto.getId());
         team.setName(dto.getName());
         team.setPlayers(dto.getPlayers().stream().map(PlayerMapper::toPlayer).toList());
-        team.setCoach(CoachMapper.toCoach(dto.getCoach()));
+        team.setCoach(coachService.getCoachById(dto.getCoachId()));
         return team;
     }
 }
