844ffc99e7b6bb8156c1a050abe73b688f1bbd67
mohamed youssef ben chehida <53410930+youssefbenchehida@users.noreply.github.com>
2023-12-21 17:15:53 +0100

    added Stadium Mapper

---
+++ BackEnd-MatchesService/src/main/java/tn/iit/dto/mapper/StadiumMapper.java
@@ -0,0 +1,22 @@
+package tn.iit.dto.mapper;
+
+import tn.iit.dto.StadiumDto;
+import tn.iit.entity.Stadium;
+
+public class StadiumMapper {
+    public static Stadium toStadium(StadiumDto stadiumDto) {
+        Stadium stadium = new Stadium();
+        stadium.setId(stadiumDto.getId());
+        stadium.setName(stadiumDto.getName());
+        stadium.setCapacity(stadiumDto.getCapacity());
+        return stadium;
+    }
+
+    public static StadiumDto toStadiumDto(Stadium stadium) {
+        return StadiumDto.builder().
+                id(stadium.getId()).
+                name(stadium.getName()).
+                capacity(stadium.getCapacity()).
+                build();
+    }
+}
