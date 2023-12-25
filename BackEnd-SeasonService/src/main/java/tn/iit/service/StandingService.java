7e7fcd6505d20e432b5c743ff22bfced00c7e7c4
Marwen Kammoun <marwenkammoun3@gmail.com>
2023-12-25 20:03:47 +0100

    update StandingService

--- BackEnd-SeasonService/src/main/java/tn/iit/service/StandingService.java
+++ BackEnd-SeasonService/src/main/java/tn/iit/service/StandingService.java
@@ -5,18 +5,29 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import tn.iit.dao.StandingRepository;
+import tn.iit.entity.Season;
 import tn.iit.entity.Standing;
+import tn.iit.utils.checks.StandingControl;
 
 @Service
 public class StandingService {
     private final StandingRepository standingRepository;
+    private final SeasonService seasonService;
 
     @Autowired
-    public StandingService(StandingRepository standingRepository) {
+    public StandingService(StandingRepository standingRepository, SeasonService seasonService) {
         this.standingRepository = standingRepository;
+        this.seasonService = seasonService;
     }
 
-    public Standing createStanding(Standing standing) {
+    public Standing createStanding(Standing standing, Long seasonId) {
+        if (!StandingControl.checkStanding(standing).checkRank().checkScore().finish()) {
+            throw new IllegalArgumentException("Invalid standing");
+        }
+
+        Season season = seasonService.getSeasonById(seasonId);
+        standing.setSeason(season);
+
         return standingRepository.save(standing);
     }
 
