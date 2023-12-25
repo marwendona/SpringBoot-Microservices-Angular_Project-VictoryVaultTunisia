9316206b2e015b1813a00dae19ece7526a3b4415
Marwen Kammoun <marwenkammoun3@gmail.com>
2023-12-25 02:39:19 +0100

    update SeasonMapper

--- BackEnd-SeasonService/src/main/java/tn/iit/dto/mapper/SeasonMapper.java
+++ BackEnd-SeasonService/src/main/java/tn/iit/dto/mapper/SeasonMapper.java
@@ -4,17 +4,21 @@ import tn.iit.dto.SeasonDto;
 import tn.iit.entity.Season;
 
 public class SeasonMapper {
+    public static SeasonDto toSeasonDto(Season season) {
+        return SeasonDto.builder()
+                .id(season.getId())
+                .name(season.getName())
+                .rounds(RoundMapper.toRoundDtoList(season.getRounds()))
+                .generalStanding(StandingMapper.toStandingDtoList(season.getGeneralStanding()))
+                .build();
+    }
+
     public static Season toSeason(SeasonDto seasonDto) {
         Season season = new Season();
         season.setId(seasonDto.getId());
         season.setName(seasonDto.getName());
+        season.setRounds(RoundMapper.toRoundList(seasonDto.getRounds()));
+        season.setGeneralStanding(StandingMapper.toStandingList(seasonDto.getGeneralStanding()));
         return season;
-    }
-
-    public static SeasonDto toSeasonDto(Season season) {
-        return SeasonDto.builder().
-                id(season.getId()).
-                name(season.getName()).
-                build();
     }
 }
