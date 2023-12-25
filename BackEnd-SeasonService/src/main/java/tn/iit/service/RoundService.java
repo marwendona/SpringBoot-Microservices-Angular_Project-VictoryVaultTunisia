661716c62c16393ee25167e74c4b354b236d846f
Marwen Kammoun <marwenkammoun3@gmail.com>
2023-12-25 02:47:01 +0100

    update RoundService

--- BackEnd-SeasonService/src/main/java/tn/iit/service/RoundService.java
+++ BackEnd-SeasonService/src/main/java/tn/iit/service/RoundService.java
@@ -6,17 +6,28 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import tn.iit.dao.RoundRepository;
 import tn.iit.entity.Round;
+import tn.iit.entity.Season;
+import tn.iit.utils.checks.RoundControl;
 
 @Service
 public class RoundService {
     private final RoundRepository roundRepository;
+    private final SeasonService seasonService;
 
     @Autowired
-    public RoundService(RoundRepository roundRepository) {
+    public RoundService(RoundRepository roundRepository, SeasonService seasonService) {
         this.roundRepository = roundRepository;
+        this.seasonService = seasonService;
     }
 
-    public Round createRound(Round round) {
+    public Round createRound(Round round, Long seasonId) {
+        if (!RoundControl.checkRound(round).checkName().checkRoundNumber().finish()) {
+            throw new IllegalArgumentException("Invalid round");
+        }
+
+        Season season = seasonService.getSeasonById(seasonId);
+        round.setSeason(season);
+
         return roundRepository.save(round);
     }
 
