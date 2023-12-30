73d7f241ea1c86a2827437d383cd6a1da81b9ea6
mohamed youssef ben chehida <53410930+youssefbenchehida@users.noreply.github.com>
2023-12-30 23:56:23 +0100

    add MatchDto and connected it to RoundDto

--- BackEnd-SeasonService/src/main/java/tn/iit/dto/RoundDto.java
+++ BackEnd-SeasonService/src/main/java/tn/iit/dto/RoundDto.java
@@ -4,6 +4,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.extern.jackson.Jacksonized;
 
+import java.util.List;
+
 @Data
 @Builder
 @Jacksonized
@@ -11,4 +13,5 @@ public class RoundDto {
     private Long id;
     private String name;
     private int roundNumber;
+    private List<MatchDto> matches;
 }
