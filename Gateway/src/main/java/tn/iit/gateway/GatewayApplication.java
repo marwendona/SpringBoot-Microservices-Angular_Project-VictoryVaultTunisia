009c2758f97a806310361dd87a059239a9723bd0
mohamed youssef ben chehida <53410930+youssefbenchehida@users.noreply.github.com>
2024-01-09 20:59:15 +0100

    add swagger

--- Gateway/src/main/java/tn/iit/gateway/GatewayApplication.java
+++ Gateway/src/main/java/tn/iit/gateway/GatewayApplication.java
@@ -32,4 +32,5 @@ public class GatewayApplication {
         urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
         return new CorsFilter(urlBasedCorsConfigurationSource);
     }
+
 }
